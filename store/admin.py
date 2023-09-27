928be71fbaea0f1900e9b0c2a9b569c99bc831d9
ahnge <ahnge226@gmail.com>
2023-09-27 16:18:07 +0630

    fixed models for sizes and colors rs

--- store/admin.py
+++ store/admin.py
@@ -5,7 +5,6 @@ from .models import (
     Order,
     OrderItem,
     Product,
-    ProductColorSize,
     ProductImage,
     ShippingAddress,
     Size,
@@ -13,17 +12,16 @@ from .models import (
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["name", "price"]
+    list_display = ["name", "price", "created_at"]
     list_filter = ["categories"]
-    search_fields = ["title", "description"]
+    search_fields = ["name", "description"]
 
 
 admin.site.register(Category)
 admin.site.register(Color)
+admin.site.register(Size)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductColorSize)
 admin.site.register(ProductImage)
 admin.site.register(ShippingAddress)
-admin.site.register(Size)
--- store/models.py
+++ store/models.py
@@ -36,20 +36,14 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     images = models.ManyToManyField("ProductImage", related_name="product", blank=True)
+    sizes = models.ManyToManyField("Size", blank=True)
+    colors = models.ManyToManyField("Color", blank=True)
     categories = models.ManyToManyField(Category)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-
-class ProductColorSize(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    color = models.ForeignKey(Color, on_delete=models.CASCADE)
-    size = models.ForeignKey(Size, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.product} - {self.color} - {self.size}"
 
 
 class Order(models.Model):
@@ -57,8 +51,8 @@ class Order(models.Model):
     guest_name = models.CharField(max_length=100, null=True, blank=True)
     guest_email = models.EmailField(null=True, blank=True)
     is_guest_order = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order #{self.pk} - {self.user.username}"
