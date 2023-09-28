59d3a7c3e27a4f40e9ea61ce4364074d1614b98e
ahnge <ahnge226@gmail.com>
2023-09-28 10:53:25 +0630

    serve media during developemnt

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.site.site_header = "Treasure Store Administration"
 admin.site.site_title = "Treasure Store"
 admin.site.index_title = "Welcome to Treasure Store Administration"
@@ -11,3 +14,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("", include("store.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
