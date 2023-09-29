4b9ce6291ceaa31276603352889b2b4f82e47c27
ahnge <ahnge226@gmail.com>
2023-09-29 08:51:57 +0630

    product detail page layout

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "store"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:slug>/", views.product_detail_view, name="product_detail"),
+]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, ProductColor
 
 
 def index(request):
     products = Product.objects.all()[:5]
     context = {"products": products}
     return render(request, "store/index.html", context)
+
+
+def product_detail_view(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    product_colors = ProductColor.objects.filter(product=product)
+    print(product_colors)
+    print(product_colors.first().images.all())
+    context = {"product": product, "product_colors": product_colors}
+    return render(request, "store/product_detail.html", context)
