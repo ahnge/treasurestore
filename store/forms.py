ec57df3d7532890c2d2f9044399e3b55b20737f5
ahnge <ahnge226@gmail.com>
2023-10-13 10:16:46 +0630

    make all form fields required and clean ph no

--- store/forms.py
+++ store/forms.py
@@ -7,6 +7,22 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ["guest_name", "guest_phone_number"]
 
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+
+        self.fields["guest_name"].required = True
+        self.fields["guest_phone_number"].required = True
+
+    def clean_guest_phone_number(self):
+        guest_phone_number = self.cleaned_data.get("guest_phone_number", "")
+        guest_phone_number = guest_phone_number.replace(" ", "")
+        return guest_phone_number
+
+    def save(self, commit=True):
+        # Clean the guest_phone_number field before saving
+        self.cleaned_data["guest_phone_number"] = self.clean_guest_phone_number()
+        return super(OrderForm, self).save(commit)
+
 
 class ShippingAddressForm(forms.ModelForm):
     class Meta:
