01609d021ca8a8eee2bb85cfe411cd708b72c103
ahnge <ahnge226@gmail.com>
2023-10-16 08:34:08 +0630

    Clean accepted_terms for orderForm
    - override clean_accpted_terms method in orderForm

--- store/forms.py
+++ store/forms.py
@@ -5,12 +5,20 @@ from .models import Order, ShippingAddress
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["guest_name", "guest_phone_number"]
+        fields = ["guest_name", "guest_phone_number", "accepted_terms"]
 
     def clean_guest_phone_number(self):
         guest_phone_number = self.cleaned_data.get("guest_phone_number", "")
         guest_phone_number = guest_phone_number.replace(" ", "")
         return guest_phone_number
+
+    def clean_accepted_terms(self):
+        accepted_terms = self.cleaned_data.get("accepted_terms")
+        if not accepted_terms:
+            raise forms.ValidationError(
+                "You must accept the terms and conditions to place an order."
+            )
+        return accepted_terms
 
     def save(self, commit=True):
         # Clean the guest_phone_number field before saving
